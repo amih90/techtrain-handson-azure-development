3d9d26d512c7833186927167f35d976140780620
Ami Hollander <amhollan@microsoft.com>
2023-12-14 15:26:51 +0000

    8️⃣ use dependency injection in your functionapp

---
+++ src/backend/Startup.cs
@@ -0,0 +1,16 @@
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(backend.Startup))]
+
+namespace backend
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            builder.Services.AddSingleton<Healthz>();
+        }
+    }
+}
